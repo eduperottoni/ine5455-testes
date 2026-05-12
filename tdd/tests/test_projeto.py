2192a35da9c6123b8e89c35af2b47645f15a6430
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 10:18:29 -0300

    tdd-12

--- tdd/tests/test_projeto.py
+++ tdd/tests/test_projeto.py
@@ -1,5 +1,6 @@
 import unittest
 from projeto import Projeto
+from funcionario import Funcionario
 
 
 class TestProjeto(unittest.TestCase):
@@ -11,3 +12,27 @@ class TestProjeto(unittest.TestCase):
     def test_criar_projeto_sem_nome(self):
         with self.assertRaises(TypeError):
             _ = Projeto()
+
+    def test_add_funcionario_no_projeto(self):
+        projeto = Projeto("Projeto Teste")
+        funcionario = Funcionario("Eduardo")
+
+        projeto.adicionar_funcionario(funcionario)
+        assert projeto.funcionarios[0].nome == "Eduardo"
+
+    def test_add_varios_funcionarios_no_projeto(self):
+        projeto = Projeto("Projeto para testes")
+        funcionario1 = Funcionario("Matheus")
+        funcionario2 = Funcionario("Joice")
+
+        projeto.adicionar_funcionario(funcionario1)
+        projeto.adicionar_funcionario(funcionario2)
+
+        assert projeto.funcionarios[0].nome == "Matheus"
+        assert projeto.funcionarios[1].nome == "Joice"
+
+    def test_add_funcionario_null(self):
+        projeto = Projeto("Projeto Teste")
+
+        with self.assertRaises(ValueError):
+            projeto.adicionar_funcionario(None)
