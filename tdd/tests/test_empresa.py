02606c1896d9561153853a64f6d09137b5cc4814
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 10:05:29 -0300

    tdd-6

--- tdd/tests/test_empresa.py
+++ tdd/tests/test_empresa.py
@@ -1,5 +1,6 @@
 import unittest
 from empresa import Empresa
+from funcionario import Funcionario
 
 
 class TestEmpresa(unittest.TestCase):
@@ -11,3 +12,11 @@ class TestEmpresa(unittest.TestCase):
     def test_criar_empresa_sem_nome(self):
         with self.assertRaises(TypeError):
             Empresa()
+
+    def test_add_funcionario(self):
+        empresa = Empresa("Companhia do Código")
+        funcionario = Funcionario("Hugo")
+
+        empresa.adicionar_funcionario(funcionario)
+
+        self.assertEqual(empresa.funcionarios[0].nome, "Hugo")
