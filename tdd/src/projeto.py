b968864c2398072871e4a790c183b5fd98e631cd
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 10:20:13 -0300

    tdd-13

--- tdd/src/projeto.py
+++ tdd/src/projeto.py
@@ -1,3 +1,12 @@
+from funcionario import Funcionario
+
+
 class Projeto:
     def __init__(self, nome):
         self.nome = nome
+        self.funcionarios = []
+
+    def adicionar_funcionario(self, funcionario):
+        if funcionario is None:
+            raise ValueError("O funcionário não pode ser nulo.")
+        self.funcionarios.append(funcionario)
