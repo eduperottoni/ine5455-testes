f3c1600ba767ec51bdb7665ed33098ebe82d445a
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 10:13:10 -0300

    tdd-11

--- tdd/src/empresa.py
+++ tdd/src/empresa.py
@@ -1,10 +1,15 @@
 from funcionario import Funcionario
+from projeto import Projeto
 
 
 class Empresa:
     def __init__(self, nome):
         self.nome = nome
         self.funcionarios = []
+        self.projetos = []
 
     def adicionar_funcionario(self, funcionario: Funcionario):
         self.funcionarios.append(funcionario)
+
+    def adicionar_projeto(self, projeto: Projeto):
+        self.projetos.append(projeto)
