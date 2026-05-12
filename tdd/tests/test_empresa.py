3fc2fd7dae272c764243d543f0c3368269d7f10e
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 09:55:32 -0300

    tdd-3

--- tdd/tests/test_empresa.py
+++ tdd/tests/test_empresa.py
@@ -7,3 +7,7 @@ class TestEmpresa(unittest.TestCase):
     def test_criar_empresa(self):
         empresa = Empresa("Companhia do Código")
         self.assertEqual(empresa.nome, "Companhia do Código")
+
+    def test_criar_empresa_sem_nome(self):
+        with self.assertRaises(TypeError):
+            Empresa()
