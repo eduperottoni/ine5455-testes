df3f8bd8306b6c78a5a7cc9aa35ead161e7fabfb
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-12 10:06:46 -0300

    tdd-7

--- tdd/src/empresa.py
+++ tdd/src/empresa.py
@@ -1,3 +1,10 @@
+from funcionario import Funcionario
+
+
 class Empresa:
     def __init__(self, nome):
         self.nome = nome
+        self.funcionarios = []
+
+    def adicionar_funcionario(self, funcionario: Funcionario):
+        self.funcionarios.append(funcionario)
