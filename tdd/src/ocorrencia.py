84c53ce4d9ae917fcae47806b17d2bf9db67815b
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-19 08:57:12 -0300

    tdd-26

--- tdd/src/ocorrencia.py
+++ tdd/src/ocorrencia.py
@@ -3,7 +3,6 @@ from funcionario import Funcionario
 
 class Ocorrencia:
     def __init__(self, resp: Funcionario):
-        pass
-        # if not resp or not isinstance(resp, Funcionario):
-        #     raise ValueError("responsável inválido")
-        # self.responsavel = resp
+        if not resp or not isinstance(resp, Funcionario):
+            raise ValueError("responsável inválido")
+        self.responsavel = resp
