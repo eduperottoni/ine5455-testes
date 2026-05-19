41a2ad03b461daf7a67974eca35905cf61a6f4a7
Eduardo Perottoni <edu.perottoni@gmail.com>
2026-05-19 09:27:43 -0300

    tdd-34

--- tdd/src/ocorrencia.py
+++ tdd/src/ocorrencia.py
@@ -5,6 +5,10 @@ class Ocorrencia:
     def __init__(self, resp: Funcionario, chave: int, resumo: str):
         if not resp or not isinstance(resp, Funcionario):
             raise ValueError("responsável inválido")
+
+        if len(resumo) < 10:
+            raise ValueError("resumo inválido")
+
         self.responsavel = resp
         self.chave = chave
         self.resumo = resumo
